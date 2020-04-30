514303240fe8e172fad8ef31ff76633a67b09aa7
Sachin Choudhary <sachin.choudhary@punchh.com>
2020-04-30 18:24:50 +0530

    initial commit

---
+++ lib/relation_document_bridge.rb
@@ -0,0 +1,6 @@
+require "relation_document_bridge/version"
+
+module RelationDocumentBridge
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/relation_document_bridge/version.rb
@@ -0,0 +1,3 @@
+module RelationDocumentBridge
+  VERSION = "0.1.0"
+end
